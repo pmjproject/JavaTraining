c4b5249558571bceb45a23d5c281ded5c2c3d51c
pmjproject <jayakodymangalya@gmail.com>
2020-01-05 23:01:02 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,20 +1,29 @@
 
 import java.util.Scanner;
 
-public class ReverseNumber {
-	private static Scanner sc;
-	
-	public static void main(String[] args) {
-		int Number, Reminder, Reverse;
-		sc = new Scanner(System.in);
-		
-		System.out.println("\n Enter Number : ");
-		Number = sc.nextInt();
-		
-		for(Reverse = 0; Number > 0; Number = Number/10) {
-			Reminder = Number %10;
-			Reverse = Reverse * 10 + Reminder;
-		}
-		System.out.format("\n Reverse of entered number is = %d\n", Reverse);
+public class Main
+{
+
+public static void main(String[] args) {
+   
+   int reverseN = 0;
+   int mod,number;
+   
+Scanner sc = new Scanner(System.in);
+
+System.out.print("Enter Number : ");
+
+number = sc.nextInt(); //456
+
+while(number>0){
+   
+   mod = number%10;
+   
+   number = number/10;//45
+   
+   reverseN = reverseN*10 + mod;
 }
+
+System.out.format("Reverse of entered number is = %d\n", reverseN);
+    }
 }
