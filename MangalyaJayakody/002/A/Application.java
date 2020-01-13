6ebe17a8ddb438e3025d5a10be12516de38e29b4
pmjman1212 <jayakodymangalya@gmail.com>
2020-01-13 21:06:21 +0530

    Classes in seperate folders and importing package

---
+++ MangalyaJayakody/002/A/Application.java
@@ -0,0 +1,13 @@
+package A;
+import C.Student;
+
+public class Application
+{ 
+    
+    public static void main(String args[]) 
+    { 
+
+        Student student= new Student();
+        student.print2(); 
+    } 
+}
