2ac84274c1e00cad24414575c813ca19211567e8
man <jayakodymangalya@gmail.com>
2020-03-01 20:05:41 +0530

    Updated oauth with mysql

---
+++ AuthMysqlNew/src/test/java/com/pmj/AuthMysqlNew/AuthMysqlNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmj.AuthMysqlNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMysqlNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
